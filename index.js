e0b9aa842c9a260fecd7d82934c9249ccdea0c0e
Urjit Sharma <urjitsharma12@gmail.com>
2022-05-04 21:41:06 -0700

    Implemented user login and logout

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const express = require('express')
+const express = require('express');
+const session = require("express-session");
+const fs = require("fs");
+
+
+const app = express()
+// const mysql = require('mysql');
+
+// const con = mysql.createConnection({
+//     host: 'us-cdbr-east-05.cleardb.net',
+//     user: 'ba23755ea94897',
+//     password: '000cc533',
+//     database: 'heroku_8cd9f1c41e90292'
+// })
 
 // Going to be used for heroku
 let http = require('http');
@@ -6,15 +19,150 @@ let url = require('url');
 const https = require('https');
 
 
-const app = express()
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
-app.use(express.static('./public'));
 
+app.use(express.static('./public'), session({
+    secret: "extra text that no one will guess",
+    name: "wazaSessionID",
+    resave: false,
+    saveUninitialized: true
+}));
+app.get("/main", function (req, res) {
+    if (!req.session.loggedIn) {
+        res.redirect("/");
+    }
 
+});
+app.get("/loginpage", function (req, res) {
+    let profile = fs.readFileSync("./public/login.html", "utf8");
+
+    res.send(profile);
+
+});
+app.get("/logout", function (req, res) {
+
+    if (req.session) {
+        req.session.destroy(function (error) {
+            if (error) {
+                res.status(400).send("Unable to log out")
+            } else {
+                // session deleted, redirect to home
+                res.redirect("/");
+            }
+        });
+    }
+});
+// Notice that this is a "POST"
+app.post("/login", function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+
+    authenticate(req.body.email, req.body.password,
+        function (userRecord) {
+            if (userRecord == null) {
+                res.send({
+                    status: "fail",
+                    msg: "User account not found."
+                });
+            } else {
+                req.session.loggedIn = true;
+                req.session.email = userRecord.email;
+                req.session.name = userRecord.name;
+                res.send({
+                    status: "success",
+                    msg: "Logged in."
+                });
+            }
+        });
+
+});
+app.post("/signup", function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+
+    const mysql = require("mysql2");
+    const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "bridgethegap"
+    });
+    connection.connect();
+    let userRecords = "insert into user (name, email, password) values ?";
+    let recordValues = [
+        [req.body.uname, req.body.email, req.body.password]
+    ];
+    connection.query(userRecords, [recordValues]);
+
+    connection.query("SELECT * FROM user",
+        function (error, results, fields) {
+            if (error) {
+                console.log(error);
+            }
+            console.log(results);
+        }
+    );
+});
+
+function authenticate(email, pwd, callback) {
+
+    const mysql = require("mysql2");
+    const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "bridgethegap"
+    });
+    connection.connect();
+    connection.query(
+        //'SELECT * FROM user',
+        "SELECT * FROM user WHERE email = ? AND password = ?", [email, pwd],
+        function (error, results, fields) {
+
+            if (error) {
+                console.log(error);
+            }
+            if (results.length > 0) {
+                // email and password found
+                return callback(results[0]);
+            } else {
+                // user not found
+                return callback(null);
+            }
+
+        }
+    );
+
+}
+async function init() {
+    const mysql = require("mysql2/promise");
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        multipleStatements: true
+    });
+    const createDBAndTables = `CREATE DATABASE IF NOT EXISTS bridgethegap;
+        use bridgethegap;
+        CREATE TABLE IF NOT EXISTS user (
+        ID int NOT NULL AUTO_INCREMENT,
+        name varchar(30),
+        email varchar(30),
+        password varchar(30),
+        PRIMARY KEY (ID));`;
+    await connection.query(createDBAndTables);
+
+}
 // process.env.PORT is the port Heroku gives
-app.listen(process.env.PORT || 3000, function (err) {
-    if (err)
-        console.log(err);
-})
+app.listen(process.env.PORT || 3000, init);
+
+
+
+
+
+
+
---
+++ public/javascript/login.js
@@ -0,0 +1,108 @@
+$(document).ready(function () {
+
+    function ajaxPOST(url, callback, data) {
+        let params = typeof data == 'string' ? data : Object.keys(data).map(
+            function (k) {
+                return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+            }
+        ).join('&');
+        console.log("params in ajaxPOST", params);
+
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                callback(this.responseText);
+
+            } else {
+                console.log(this.status);
+            }
+        }
+        xhr.open("POST", url);
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send(params);
+    }
+
+    function beginPostLogin() {
+        let email = document.getElementById("login_email");
+        let password = document.getElementById("password");
+        let queryString = "email=" + email.value + "&password=" + password.value;
+
+        if (email.value.length > 0 || password.value.length > 0) {
+            ajaxPOST("/login", function (data) {
+
+                if (data) {
+                    let dataParsed = JSON.parse(data);
+                    if (dataParsed.status == "fail") {
+                        document.getElementById("errorMsg").innerHTML = dataParsed.msg;
+                    } else {
+                        window.location.replace("main.html");
+                    }
+                }
+            }, queryString);
+        } else {
+            document.getElementById("errorMsg").innerHTML = "Please fill out the Fields";
+        }
+
+    }
+
+    function beginPostSignUp() {
+        let email = document.getElementById("login_email");
+        let password = document.getElementById("password");
+        var user_name_input = document.getElementById("user_name")
+        let queryString = "email=" + email.value + "&password=" + password.value + "&uname=" + user_name_input.value;
+
+        if (email.value.length > 0 || password.value.length > 0 || user_name_input.value.length > 0) {
+            ajaxPOST("/signup", function (data) {
+
+                if (data) {
+                    let dataParsed = JSON.parse(data);
+                    if (dataParsed.status == "fail") {
+                        document.getElementById("errorMsg").innerHTML = dataParsed.msg;
+                    } else {
+                        window.location.replace("main.html");
+                    }
+                }
+            }, queryString);
+        } else {
+            document.getElementById("errorMsg").innerHTML = "Please fill out the Fields";
+        }
+    }
+
+    document.querySelector("#login_button").addEventListener("click", function (e) {
+        e.preventDefault();
+
+        var sign_up_button = document.getElementById("sign_up");
+        if (sign_up_button.textContent == 'Sign Up?') {
+            beginPostLogin();
+        } else {
+            beginPostSignUp();
+        }
+    });
+
+
+    document.querySelector("#sign_up").addEventListener("click", function (e) {
+        document.getElementById("errorMsg").innerHTML = "";
+        var sign_up_button = document.getElementById("sign_up");
+        var log_in_button = document.getElementById("login_button");
+        var user_name_input = document.getElementById("user_name")
+
+        var currently_changing_text = false
+        if (!currently_changing_text & sign_up_button.textContent == 'Sign Up?') {
+            currently_changing_text = true
+            user_name_input.style.display = 'inline-block'
+            sign_up_button.textContent = 'Log In?'
+            log_in_button.value = 'Sign Up'
+        }
+
+        if (!currently_changing_text & sign_up_button.textContent == 'Log In?') {
+            currently_changing_text = true
+            user_name_input.style.display = 'none'
+            sign_up_button.textContent = 'Sign Up?'
+            log_in_button.value = 'Log In'
+        }
+        currently_changing_text = false
+
+    });
+
+});
